1b9c5c36f220f0bc3abf46df58d60ae8551120e5
kevindong <yukenakisa@gmail.com>
2022-09-30 00:25:03 +0800

    add rwd

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,20 @@
       <h1 class="p1">There is no one</h1>
       <h2 class="p2">who loves pain</h2>
       <!-- <img src="./assets/白色海浪.png" alt="" class="back" /> -->
-      <img src="./assets/form.png" alt="" class="btn_form" />
+      <a href="#anchor" class="btn_form"
+        ><img src="./assets/form.png" alt=""
+      /></a>
       <div class="info">
+        <div class="sup supB">
+          <img src="./assets/sup.png" alt="" class="supB1" />
+          <img src="./assets/船槳.png" alt="" class="supB2" />
+          <img src="./assets/泡泡.png" alt="" class="supB3" />
+          <img src="./assets/泡泡-1.png" alt="" class="supB4" />
+        </div>
+        <div class="sup supF">
+          <img src="./assets/sup煙火.png" alt="" class="supF1" />
+          <img src="./assets/煙火.png" alt="" class="supF2" />
+        </div>
         <div class="title">
           <img src="./assets/cactus.png" alt="" />
           <p class="pt1p">paragraph</p>
@@ -41,8 +53,9 @@
     </div>
     <div class="part2">
       <div class="inn2">
-        <img src="./assets/Frame18.png" alt="" class="Frame18" />
+        <img src="./assets/Frame18.png" alt="" class="Frame18" id="anchor" />
         <div class="form">
+          <img src="./assets/turtle.png" alt="" class="turtle" />
           <div class="sub">
             <label name="store">store</label>
             <div class="warn">*</div>
@@ -54,9 +67,12 @@
             placeholder="placeholder text"
             class="search"
             v-model="storeVal"
-            v-bind:class="{ err:  !storeValid}"
+            v-bind:class="{ err: !storeValid }"
           />
-          <div class="error" v-if="!storeValid">wrongformat</div>
+          <div class="error" v-if="!storeValid && storeVal !== ''">
+            wrongformat
+          </div>
+          <div class="error" v-if="!storeValid && storeVal == ''">required</div>
           <datalist id="typelist">
             <option value="store1">store1</option>
             <option value="store2">store2</option>
@@ -71,9 +87,12 @@
             type="text"
             placeholder="placeholder text"
             v-model="nameVal"
-            v-bind:class="{ err:  !nameValid}"
+            v-bind:class="{ err: !nameValid }"
           />
-          <div class="error" v-if="!nameValid">wrongformat</div>
+          <div class="error" v-if="!nameValid && nameVal !== ''">
+            wrongformat
+          </div>
+          <div class="error" v-if="!nameValid && nameVal == ''">required</div>
           <div class="sub">
             <label name="phone">phone</label>
             <div class="warn">*</div>
@@ -84,9 +103,12 @@
             placeholder="placeholder text"
             pattern="[0-9]{4}-[0-9]{3}-[0-9]{3}"
             v-model="phoneVal"
-            v-bind:class="{ err:  !phoneValid}"
+            v-bind:class="{ err: !phoneValid }"
           />
-          <div class="error" v-if="!phoneValid">wrongformat</div>
+          <div class="error" v-if="!phoneValid && phoneVal !== ''">
+            wrongformat
+          </div>
+          <div class="error" v-if="!phoneValid && phoneVal == ''">required</div>
           <div class="sub">
             <label name="Amount">Amount of consumption</label>
             <div class="warn">*</div>
@@ -96,9 +118,14 @@
             type="text"
             placeholder="placeholder text"
             v-model="amountVal"
-            v-bind:class="{ err:  !amountValid}"
+            v-bind:class="{ err: !amountValid }"
           />
-          <div class="error" v-if="!amountValid">wrongformat</div>
+          <div class="error" v-if="!amountValid && amountVal !== ''">
+            wrongformat
+          </div>
+          <div class="error" v-if="!amountValid && amountVal == ''">
+            required
+          </div>
           <div class="sub">
             <label name="payment">payment</label>
             <div class="warn">*</div>
@@ -109,9 +136,14 @@
             list="typelist2"
             placeholder="placeholder text"
             v-model="paymentVal"
-            v-bind:class="{ err:  !paymentValid}"
+            v-bind:class="{ err: !paymentValid }"
           />
-          <div class="error" v-if="!paymentValid">wrongformat</div>
+          <div class="error" v-if="!paymentValid && paymentVal !== ''">
+            wrongformat
+          </div>
+          <div class="error" v-if="!paymentValid && paymentVal == ''">
+            required
+          </div>
           <datalist id="typelist2">
             <option value="digital payment">digital payment</option>
             <option value="ATM">ATM</option>
@@ -119,10 +151,9 @@
         </div>
         <div class="submit" @click="submitted">
           <div class="submit-img">
-            <img src="./assets/submit.png" v-if="!ifSubmit">
-            <img src="./assets/Text.png" v-if="susses">
-            <img src="./assets/TextF.png" v-if="showF">
-
+            <img src="./assets/submit.png" v-if="!ifSubmit" />
+            <img src="./assets/Text.png" v-if="susses" />
+            <img src="./assets/TextF.png" v-if="showF" />
           </div>
         </div>
         <div class="container">
@@ -154,7 +185,7 @@
     </div>
 
     <div class="inn3">
-      <div class="wod">Neque porro quisquam<br />est qui dolorem!</div>
+      <div class="wod"><br />Neque porro quisquam<br />est qui dolorem!</div>
       <img src="./assets/market.png" alt="" class="market" />
     </div>
   </div>
@@ -257,7 +288,6 @@ export default {
     amountVal: "amountCheck",
     paymentVal: "paymentCheck",
     ifSubmit: "submittedCheck",
-    
   },
   methods: {
     storeCheck() {
@@ -293,21 +323,27 @@ export default {
       }
     },
     amountCheck() {
-      if (!isNaN(this.amountVal) && this.amountVal >0) {
+      if (!isNaN(this.amountVal) && this.amountVal > 0) {
         this.amountValid = true;
       } else {
         this.amountValid = false;
       }
     },
-    paymentCheck(){
-      if (this.paymentVal == "digital payment" || this.paymentVal == "ATM" ) {
+    paymentCheck() {
+      if (this.paymentVal == "digital payment" || this.paymentVal == "ATM") {
         this.paymentValid = true;
       } else {
         this.paymentValid = false;
       }
     },
     submitted() {
-      if (this.storeVal == "" || this.nameVal == "" || this.phoneVal == "" || this.amountVal == "" || this.paymentVal == "") {
+      if (
+        this.storeVal == "" ||
+        this.nameVal == "" ||
+        this.phoneVal == "" ||
+        this.amountVal == "" ||
+        this.paymentVal == ""
+      ) {
         this.ifSubmit = false;
         this.storeValid = false;
         this.nameValid = false;
@@ -315,15 +351,21 @@ export default {
         this.amountValid = false;
         this.paymentValid = false;
       } else {
-      this.ifSubmit = true;
+        this.ifSubmit = true;
       }
     },
-    submittedCheck(){
-      if (this.storeValid == true && this.nameValid == true && this.phoneValid == true && this.amountValid == true && this.paymentValid == true) {
+    submittedCheck() {
+      if (
+        this.storeValid == true &&
+        this.nameValid == true &&
+        this.phoneValid == true &&
+        this.amountValid == true &&
+        this.paymentValid == true
+      ) {
         this.susses = true;
       } else {
         this.susses = false;
-        this.showF =  true;
+        this.showF = true;
       }
     },
   },
@@ -346,6 +388,7 @@ html {
   background-image: url("./assets/底色.png");
   position: relative;
   display: flex;
+  scroll-behavior: smooth;
 }
 
 body {
@@ -427,6 +470,42 @@ body {
       height: 322px;
       background-color: #dae4f1;
 
+      .sup {
+        position: absolute;
+      }
+      .supB {
+        right: 11%;
+        top: 33%;
+        .supB2 {
+          position: absolute;
+          right: 0px;
+          top: 13px;
+        }
+        .supB3 {
+          position: absolute;
+          right: 40%;
+          top: -28%;
+        }
+        .supB4 {
+          position: absolute;
+          right: 26%;
+          top: -65%;
+        }
+      }
+      .supF {
+        left: 15%;
+        bottom: -11%;
+
+        .supF1 {
+          width: 105%;
+          height: 105%;
+        }
+        .supF2 {
+          position: absolute;
+          bottom: 9%;
+          left: -27%;
+        }
+      }
       .title {
         position: relative;
         display: flex;
@@ -475,7 +554,7 @@ body {
     display: flex;
     top: -1px;
     width: 100%;
-    height: 180%;
+    height: 220%;
     background-color: #e2d9d3;
     margin-bottom: 0px;
 
@@ -497,10 +576,17 @@ body {
         top: 1.5%;
       }
       .form {
+        background-color: #ffffff;
         margin: 0 auto;
         border: #b57556 2px solid;
         border-radius: 16px;
         padding: 5%;
+
+        .turtle {
+          position: absolute;
+          right: 41%;
+          top: 1.2%;
+        }
 
         input {
           width: 100%;
@@ -514,11 +600,11 @@ body {
         }
 
         input:focus {
-          border: solid 3px #93BBF9;
+          border: solid 3px #93bbf9;
         }
 
         .err {
-          border: 3px solid #E06D6D;
+          border: 3px solid #e06d6d;
         }
 
         label {
@@ -528,8 +614,8 @@ body {
           margin-bottom: 3px;
         }
         .error {
-          color: #E06D6D;
-          font-family: 'Noto Sans TC';
+          color: #e06d6d;
+          font-family: "Noto Sans TC";
           font-style: normal;
           font-weight: 400;
           font-size: 12px;
@@ -572,6 +658,13 @@ body {
         left: 4%;
         margin: 0 auto;
         top: 4%;
+
+        .asd {
+          .concer {
+            position: relative;
+            top: -81px;
+          }
+        }
         .containers {
           justify-content: center;
           position: relative;
@@ -620,15 +713,18 @@ body {
             line-height: 125%;
             color: #b3b960;
             bottom: 50%;
-            left: 8%;
+            right: 14%;
+            text-align: center;
           }
-          .asd {
-            .concer {
-              bottom: -10px;
-            }
+        }
+        .zxc {
+          .connum {
+            color: #a0bcc8;
+            border: #a0bcc8 1px solid;
           }
-          .zxc {
-            bottom: -10px;
+          .conpa {
+            color: #a0bcc8;
+            right: 14%;
           }
         }
       }
@@ -683,9 +779,11 @@ body {
   .inn3 {
     position: relative;
     margin: 0 auto;
-    height: 70vh;
+    height: 75vh;
 
     .wod {
+      background-image: url("./assets/底色.png");
+      border-radius: 60px;
       position: relative;
       text-align: center;
       font-family: "Noto Sans TC";
@@ -694,12 +792,196 @@ body {
       font-size: 20px;
       line-height: 200%;
       color: #ffffff;
+      bottom: 7%;
     }
     .market {
       position: relative;
       left: 50%;
-      transform: translate(-50%, 10%);
+      transform: translate(-50%, -3%);
     }
+  }
+}
+
+@media screen and (min-width: 415px) and (max-width: 769px) {
+  body {
+    position: relative;
+    left: -8px;
+  }
+  #app {
+    max-width: 768px;
+    position: relative;
+    width: 100%;
+    height: 100vh;
+    overflow-x: hidden;
+    
+    .inn {
+      max-width: 768px;
+      justify-content: center;
+      .info {
+        width: 720px;
+        ol {
+          width: 660px;
+        }
+        .title {
+          transform: translate(-23%, 0%);
+          margin-top: 15px;
+          width: 300px;
+          margin-right: 0px;
+        }
+        .supB {
+          position: absolute;
+          right: 6%;
+        }
+        .supF {
+          left: 7%;
+        }
+      }
+    }
+
+    .waves {
+      position: relative;
+      top: 1px;
+    }
+    .part2 {
+      max-width: 768px;
+      height: 230%;
+      .inn2 {
+        max-width: 768px;
+        justify-content: center;
+        .Frame18 {
+          top: -3.6%;
+          position: relative;
+          z-index: 2;
+        }
+        .submit {
+          position: relative;
+          top:-3%
+        }
+        .container {
+          position: relative;
+          top:-1%;
+        }
+        .form {
+          max-width: 720px;
+          bottom: 5%;
+          position: relative;
+          padding: 15px;
+          .turtle {
+            position: absolute;
+            z-index: 2;
+            top: -6%;
+            right: 37%;
+          }
+          .search {
+            width: 680px;
+          }
+        }
+        .word {
+          position: relative;
+          top: -1%
+        }
+        .light {
+          position: relative;
+          top:0%;
+        }
+      }
+    }
+    .inn3 {
+      position: relative;
+      top: -11px;
+    }
+  }
+}
+
+@media screen and (max-width: 414px) {
+  body {
+    overflow-x: hidden;
+    margin: 0px;
+  }
+  #app {
+    max-width: 414px;
+
+    .inn {
+      max-width: 414px;
+      .info {
+        width: 366px;
+        height: 489px;
+
+        ol {
+          width: 305px;
+        }
+        .title {
+          transform: translate(-20%, 0%);
+          margin-top: 25px;
+        }
+        .supB {
+          position: absolute;
+          right: 1%;
+          top: 26%;
+        }
+        .supF {
+          left: 7%;
+          bottom: -7%;
+        }
+      }
+    }
+    .waves {
+      position: relative;
+      top: 1px;
+    }
+    .part2 {
+      max-width: 414px;
+      height: 230%;
+      .inn2 {
+        max-width: 414px;
+        .form {
+          width: 366px;
+          position: relative;
+          padding: 15px;
+          .turtle {
+          z-index: 3;
+          position: absolute;
+          right: 26%;
+          top: -6%;
+        }
+          .search {
+            width: 326px;
+          }
+        }
+        .Frame18 {
+          z-index: 2;
+        }
+        .container {
+          width: 365px;
+          margin: 0px;
+          padding: 0px;
+          .containers {
+            margin-left: -62px;
+            left: 68px;
+          }
+          .containers.asd.zxc {
+            width: 178px;
+            margin: 0px; 
+          }
+        }
+
+        .word {
+          position: relative;
+          top: 12%;
+        }
+        .light {
+          position: relative;
+          top: 13%;
+        }
+      }
+    }
+    .inn3 {
+        .wod {
+          position: relative;
+          letter-spacing: 0.2em;
+
+        }
+      }
   }
 }
 </style>
